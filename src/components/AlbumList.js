973958c0cebc0a6a4710e66fcdc325fa8682a6fb
zenara <zenaronline@gmail.com>
2018-10-02 11:52:54 +0530

    componentWillMount

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+//class based components can use life cycle methods
 class AlbumList extends Component {
+    //ComponentWillMount -> When component is about to render to the screen
+    componentWillMount(){
+        console.log('componentWillMount in AlbumList');
+    }
     render(){
         return (
             <View>
