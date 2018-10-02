94ca31c3df4e2f06b524ab40ac8a33394ce4581e
zenara <zenaronline@gmail.com>
2018-10-02 12:21:09 +0530

    Load API

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 //class based components can use life cycle methods
 class AlbumList extends Component {
     //ComponentWillMount -> When component is about to render to the screen
     componentWillMount(){
         console.log('componentWillMount in AlbumList');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
     }
     render(){
         return (
