38da0f90d4c906e14a8c7f42c88b4d6ab7a11d5d
zenara <zenaronline@gmail.com>
2018-10-02 10:47:35 +0530

    Header styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,35 @@
 // import libraries
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 //make component
 const Header = () =>{
-    return <Text>Albums</Text>
+    const { textStyle, viewStyle } =styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums !</Text>
+        </View>
+    );
+};
+
+
+const styles = {
+    viewStyle:{
+        backgroundColor: '#1de9b6',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.2,
+        elevation:2,
+        position: 'relative'
+    },
+    textStyle:{
+        fontSize: 25
+    }
 };
 
 //make component available to other parts of the application
