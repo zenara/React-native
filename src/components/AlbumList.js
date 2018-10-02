ea0a5d18148962f0f009db7d61a65f430004c57e
zenara <zenaronline@gmail.com>
2018-10-02 16:24:39 +0530

    Render Album array

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -10,11 +10,14 @@ class AlbumList extends Component {
         console.log('componentWillMount in AlbumList');
         axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({ albums: response.data }));
     }
+    renderAlbums(){
+        return this.state.albums.map(album =><Text>{album.title}</Text>);
+    }
     render(){
         console.log (this.state);
         return (
             <View>
-                <Text>Album List!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
