b7e9997c788cd061762d97e396a764b94b8f2514
zenara <zenaronline@gmail.com>
2018-10-02 12:48:55 +0530

    Setting the state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,12 +4,14 @@ import axios from 'axios';
 
 //class based components can use life cycle methods
 class AlbumList extends Component {
+    state = { albums: [] };
     //ComponentWillMount -> When component is about to render to the screen
     componentWillMount(){
         console.log('componentWillMount in AlbumList');
-        axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({ albums: response.data }));
     }
     render(){
+        console.log (this.state);
         return (
             <View>
                 <Text>Album List!!!</Text>
